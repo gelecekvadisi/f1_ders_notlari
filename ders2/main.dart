64d2416d0257181bc52401a884ef0f04320c3016
FurkanYagmur <yagmurfurkan425@gmail.com>
2023-11-03 09:55:05 +0300

    2. Derste işlenen kodlar

---
+++ ders2/main.dart
@@ -0,0 +1,40 @@
+void main() {
+  // String isim = "Ahmet";
+
+  // Merhaba dünya yazdıran komut
+  // print("Merhaba Ahmet!");
+
+  // Mehmet beyin sözleri aşağıdadır.
+
+  /*  nkadnglkadsingk
+  nfşksanfşsd
+  nkgşasniagsdş
+  nksaingls */
+
+  /*print("$isim \"Bugün hava sıcak\" dedi");
+  print('$isim \'Bugün hava sıcak\' dedi');*/
+
+  // print(2 + 2);
+
+  // String : içerisinde metinsel ifadeler barındırır.
+  // int : içerisinde tam sayı sayılar barındırır.
+
+  //  degiken_turu degisken_adi = 10;
+  String soyad = "Yağmur";
+  int yil = 2023;
+  double pi = 3.14;
+  double notOrtalamasi = 83.27;
+  double notOrtalamasi2 = 83.27;
+
+  //  true ise cinsiyeti erkektir.
+  bool cinsiyet = true;
+  bool mezunMu = true;
+  bool havaGunesliMi = true;
+
+  var calistigiKurum = "İhlas Vakfı";
+  var mezunYili = 2000;
+
+  print("Soyadım: $soyad Bulunduğumuz yıl: " +
+      yil.toString() +
+      " Hava güneşli mi: $havaGunesliMi");
+}
---
+++ ders2/obs.dart
@@ -0,0 +1,14 @@
+void main() {
+  String name = "Furkan Yağmur";
+  int age = 30;
+  int toplamDersSayisi = 5;
+  String memleket = "Adıyaman";
+  String ikamet = "İstanbul";
+  double notOrtalamasi = 85.21;
+  bool erkekMi = true;
+  bool mezunMu = true;
+  bool danismanVarMi = false;
+
+  print(
+      "Merhaba $name. Yaşınız: $age, Memleketiniz: $memleket, Not ortalamanız: $notOrtalamasi, Mezun olmuş mu: $mezunMu");
+}
